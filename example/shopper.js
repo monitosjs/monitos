1f9f11733d8ab21a9c44aa053aa1af90fe09c03f
Arturo Martinez Minguez <arturo@icemobile.com>
2016-09-21 11:36:52 +0200

    feat: initial version

---
+++ example/shopper.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var fs = require('fs');
+
+var Monito = require('../lib/index');
+
+const formatter = {
+    state(data, chimp)  {
+        console.log('* ' + data.message);
+    },
+    error(data) {
+        console.log('! ' + data.message);
+        if (data.err) {
+            console.log(data.err);
+        }
+    }
+};
+
+process.on('SIGINT', function () {
+    console.log('\n' + chalk.magenta.bold('Ok bye!\n'));
+    process.exit();
+});
+
+let chimp = new Monito({
+    persona: {
+        init: (monito) => {
+            monito.state = 'register';
+        },
+        states: {
+            register: (monito, next) => {
+                next(null, 'getProfile');
+            },
+            getProfile: (monito, next) => {
+                next(null, {
+                    browse: 4
+                }, 'shop');
+            },
+            browse: (monito, next) => {
+                next(null, {
+                    browse: 6
+                }, 'shop');
+            },
+            shop: (monito, next) => {
+                next(null, 'logout');
+            },
+            logout: (monito, next) => {
+                monito.stop();
+                next();
+            }
+        }
+    }
+});
+
+chimp.on('report', function (data) {
+    if (data.type && formatter[data.type]) {
+        formatter[data.type](data, chimp);
+    } else {
+        console.log(data.message);
+    }
+});
+
+chimp.start();
